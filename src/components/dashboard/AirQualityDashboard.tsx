2ff1f9cfca3c786369871eb1f5d7df7ba3251960
Cody Payne <copayne@gmail.com>
2024-09-02 19:19:48 -0400

    add initial sensor card

--- src/components/dashboard/AirQualityDashboard.tsx
+++ src/components/dashboard/AirQualityDashboard.tsx
@@ -1,16 +1,10 @@
 import React, {
   Suspense,
-  useMemo,
-  useState,
 } from 'react';
-import { useQuery } from '@apollo/client';
 import dynamic from 'next/dynamic';
+import SensorGrid from './SensorGrid';
 import Layout from '../layout/Layout';
-import { GET_SENSORS } from '../../graphql/Sensor';
-import { useSensorData } from '~/hooks/useSensorData';
-import Heatmap, { formatHeatmapData } from '../charts/Heatmap';
-import LineChart from '../charts/LineChart';
-import { useSensorDataContext } from '~/context/SensorDataContext';
+import { useSensorReadingData } from '~/hooks/useSensorReadingData';
 
 const DashboardGrid = dynamic(() => import ('../dashboard/DashboardGrid'), {
   loading: () => <p>Loading dashboard...</p>
@@ -22,40 +16,17 @@ const AirQualityDashboard = () => {
     loading,
     sensorReadings,
     updateCriteria,
-  } = useSensorData();
-  const { data: sensorData } = useQuery(GET_SENSORS);
-  const { state } = useSensorDataContext();
-  const { criteria } = state;
-  console.log(criteria);
-  console.log(sensorReadings);
+  } = useSensorReadingData();
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
 
-  const series = !!sensorData && !!sensorReadings
-    ? formatHeatmapData(sensorData.sensors, sensorReadings)
-    : [];
-
-  const handleUpdateCriteria = () => updateCriteria({ maxCO2: 700 });
-
   return (
     <Layout>
-      <div id="sensor-grid">
-        To-do
-      </div>
+      <SensorGrid />
       <Suspense fallback={<div>Loading Dashboard...</div>}>
         <DashboardGrid>
-          {/* Add data tables and visualizations here */}
-          <div>
-            <LineChart />
-            <Heatmap series={series} />
-          </div>
-          <div>
-            <button onClick={handleUpdateCriteria}>
-              Test button
-            </button>
-          </div>
+          To-do dashboard...
         </DashboardGrid>
       </Suspense>
     </Layout>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,10 +9,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="flex flex-col flex-1 overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-b from-neutral-700 to-neutral-900 relative">
+      <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto p-6 m-auto w-screen sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-2xl">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto p-6 relative z-10">
           {children}
         </main>
       </div>
